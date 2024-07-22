29ab54ebe94f037ffa41d3c59c0768a5541d7059
Tyler Corbett <thetylercorbett@gmail.com>
2024-07-22 14:39:15 -0700

    searchbar

--- app/components/Searchbar/Searchbar.jsx
+++ app/components/Searchbar/Searchbar.jsx
@@ -3,11 +3,11 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 export default function Searchbar() {
   return (
-    <div className="relative inline-block opacity-50">
-      <MagnifyingGlassIcon className="absolute left-2 top-1/2 text-lightGray -translate-y-1/2 w-4 h-4" />
+    <div className="relative inline-block focus-within:opacity-100 opacity-50 ">
+      <MagnifyingGlassIcon className="absolute left-2 top-1/2 transform -translate-y-1/2 w-4 h-4 text-white" />
       <input
         type="text"
-        className="bg-transparent border border-lightGray p-2 rounded-lg pl-8"
+        className="bg-transparent border border-gray-400 p-2 rounded-lg pl-8 text-white focus:border-white focus:outline-none peer"
       />
     </div>
   );
