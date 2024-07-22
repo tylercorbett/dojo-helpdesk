5bdf7fe67024029324676b2ed94c3169ddc1e878
Tyler Corbett <thetylercorbett@gmail.com>
2024-07-22 16:17:32 -0700

    first row of buttons

--- app/components/Settings/Settings.jsx
+++ app/components/Settings/Settings.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 export default function Settings() {
   return (
-    <div className="flex items-center w-full gap-4">
-      <h2 className="font-bold text-lg text-white mr-20 my-8">Settings</h2>
+    <div className="flex items-center w-full gap-4 flex-wrap my-8">
+      <h2 className="font-bold text-lg text-white mr-20 ">Settings</h2>
       <button className="active-wide-btn">Video</button>
       <button className="wide-btn">Game</button>
       <button className="wide-btn">Interface</button>
+      <button className="wide-btn">Keyboard</button>
+      <button className="wide-btn">Mouse</button>
+      <button className="wide-btn">Audio</button>
+      <button className="wide-btn">Chat</button>
+      <button className="wide-btn">Privacy</button>
     </div>
   );
 }
