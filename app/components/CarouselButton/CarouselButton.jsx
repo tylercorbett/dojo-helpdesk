83a9019e78484ddefa8f034476aabd9394e35588
Tyler Corbett <thetylercorbett@gmail.com>
2024-07-22 17:48:51 -0700

    fix opacities

--- app/components/CarouselButton/CarouselButton.jsx
+++ app/components/CarouselButton/CarouselButton.jsx
@@ -26,7 +26,7 @@ export default function CarouselButton({
   };
 
   return (
-    <div className="flex w-full items-center gap-4 p-3 bg-gray-600 bg-opacity-50 rounded-lg overflow-hidden">
+    <div className="flex w-full items-center gap-4 p-3 border border-gray-600 border-opacity-50 bg-opacity-50 rounded-lg overflow-hidden bg:transparent hover:bg-gray-600 hover:bg-opacity-50 hover:cursor-pointer">
       <p className="font-bold text-white w-96">{label}</p>
       <div className="flex items-center justify-between justify-self-end w-full">
         <button onClick={handlePrevious} className="carousel-button-control">
